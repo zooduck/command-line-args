66ad0cd418da4d5965dd20dd34cf4f28a72487c7
zooduck <zooduck@beniceoreatsomerice.com>
2023-05-25 19:36:41 +0100

    update package type to module and rename file extensions from .mjs to .js

--- build.js
+++ build.js
@@ -3,5 +3,5 @@ import { buildTools } from '@zooduck/build-tools';
 
 await fs.rm('dist', { recursive: true, force: true });
 await fs.mkdir('dist');
-await fs.copyFile('src/commandlineArgs.util.mjs', 'dist/index.module.mjs');
-await buildTools.removeCommentsFromFile('dist/index.module.mjs');
+await fs.copyFile('src/commandlineArgs.util.js', 'dist/index.module.js');
+await buildTools.removeCommentsFromFile('dist/index.module.js');
